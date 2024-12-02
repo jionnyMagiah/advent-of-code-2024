7c9dad069ef87bd4725b2f1feae22f77f9920ec5
Jionny Magiah <giovanni.dibari15@gmail.com>
2024-12-02 21:18:26 +0100

    day 1: js

---
+++ js/01/index.js
@@ -0,0 +1,32 @@
+import { input } from './data.js';
+console.clear();
+let obj = input
+    .split('\n')
+    .map((l) => l.split('   '))
+    .map((x) => {
+        return { l: x[0], r: x[1] };
+    });
+let lefts = obj.map((o) => o.l).sort();
+let rights = obj.map((o) => o.r).sort();
+
+let part1Res = 0;
+
+for (let i = 0; i < lefts.length; i++) {
+    part1Res += Math.abs(lefts[i] - rights[i]);
+}
+// END PART 1
+
+let sim = [];
+
+lefts.forEach((l) => {
+    let count = 0;
+    rights.forEach((r) => {
+        if (l === r) count++;
+    });
+    sim = [...sim, { value: l, count }];
+});
+
+let part2Res = sim.reduce((acc, curr) => {
+    return acc += curr.value * curr.count;
+}, 0);
+console.log(part2Res);
